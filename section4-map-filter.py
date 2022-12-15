9b83793c38fd99faa477b8ea0dd64ec3ebfd6e60
Shinyee Xiong <yingstudy@gmail.com>
2022-12-14 22:39:50 -0800

    finish LR example

--- section4-map-filter.py
+++ section4-map-filter.py
@@ -5,4 +5,14 @@ seq = [1, 2, 3, 4, 5]
 
 new_seq = list(map(times2, seq))
 
+t = lambda var: var*2
+
+print(t(60))
+
+print( list(filter(lambda num: num%2 == 0, seq)) )
+
 print(new_seq)
+
+planet = "Earth"
+diameter = 12742
+print('The diameter of {a} is {b} kilometers'.format(a = planet, b = diameter))
